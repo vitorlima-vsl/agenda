b935f84ffbe4ab476cadbd0035f73d1e6e718b2b
wndlima <wanderleylima2002@gmail.com>
2024-05-17 11:11:17 -0300

    configuracao da rota index

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContatoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/index', [ContatoController::class, 'index'])->name('contatos.index');
