34653ec9ad9349a4725b738603a5bafab5d431c9
wndlima <wanderleylima2002@gmail.com>
2024-05-17 12:45:54 -0300

    configuracao da route

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,4 @@ Route::get('/', function () {
 
 
 Route::get('/index', [ContatoController::class, 'index'])->name('contatos.index');
+Route::get('/create', [ContatoController::class, 'create'])->name('contatos.create');
