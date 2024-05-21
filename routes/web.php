f544b8a9f2b0278d9b395c3a2d37a43b1b3b71f4
wndlima <wanderleylima2002@gmail.com>
2024-05-21 10:36:24 -0300

    configuração da rota do metodo store

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,4 @@ Route::get('/', function () {
 
 Route::get('/index', [ContatoController::class, 'index'])->name('contatos.index');
 Route::get('/create', [ContatoController::class, 'create'])->name('contatos.create');
+Route::post('/store', [ContatoController::class, 'store'])->name('contatos.store');
