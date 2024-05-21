34bf91ebd0836ff90b1ff5ca888833b100112cfc
wndlima <wanderleylima2002@gmail.com>
2024-05-21 12:29:58 -0300

    rotas

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,7 @@ Route::get('/', function () {
 Route::get('/index', [ContatoController::class, 'index'])->name('contatos.index');
 Route::get('/create', [ContatoController::class, 'create'])->name('contatos.create');
 Route::post('/store', [ContatoController::class, 'store'])->name('contatos.store');
+Route::get('/edit/{id}', [ContatoController::class, 'edit'])->name('contatos.edit');
+Route::put('/update/{id}', [ContatoController::class, 'update'])->name('contatos.update');
+Route::delete('/destroy/{id}', [ContatoController::class, 'destroy'])->name('contatos.destroy');
+Route::get('/show/{id}', [ContatoController::class, 'show'])->name('contatos.show');    
