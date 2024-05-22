e6b21905349ba8bea1dc67d3b38097d60663cff6
wndlima <wanderleylima2002@gmail.com>
2024-05-22 15:42:03 -0300

    comentario adicionado na view form

--- resources/views/contatos/form.blade.php
+++ resources/views/contatos/form.blade.php
@@ -17,12 +17,13 @@
 <body class="font-sans antialiased dark:bg-black dark:text-white/50">
 
 
-    <form action="{{Route('contatos.store')}}" method="POST">
+    <form action="{{Route('contatos.store')}}" method="POST" >
         @csrf
         <label for="nome">Nome</label>
-        <input type="text" name="nome" id="nome">
+        <input type="text" name="nome" id="nome" value="{{isset($contato) ? $contato->nome : null}}" {{isset($form) ? $form : null}}>
+        {{-- quero fazer a insercao de mais numeros com JS, entao vou esperar para criar na proxima issue, mas ja esta adaptado a ter mais de um numero. --}}
         <label for="telefoneNumero">Telefone</label>
-        <input type="text" name="telefoneNumero[]" id="telefoneNumero">
+        <input type="text" name="telefoneNumero[]" id="telefoneNumero" value="">
 
         <label for="cidade">cidade</label>
         <input type="text" name="cidade" id="cidade">
