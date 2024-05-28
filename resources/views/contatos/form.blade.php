b25a4fb22ba401c485b1d7929214a35240cec65a
wndlima <wanderleylima2002@gmail.com>
2024-05-28 12:12:01 -0300

    titulos modificados

--- resources/views/contatos/form.blade.php
+++ resources/views/contatos/form.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Agenda de Contatos</title>
 
     {{-- links --}}
     <script src="https://cdn.tailwindcss.com"></script>
